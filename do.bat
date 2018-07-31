87672f10636dd4d5b7356ffd0d505c1f876f5294
Iydon <11711217@mail.sustc.edu.cn>
2018-07-31 21:35:33 +0800

    section 2

--- do.bat
+++ do.bat
@@ -1,3 +1,4 @@
 @echo off
 xelatex notes.tex
+xelatex notes.tex
 del notes.aux notes.log notes.thm notes.toc
--- do.sh
+++ do.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 xelatex notes.tex
+xelatex notes.tex
 rm notes.aux notes.log notes.thm notes.toc
